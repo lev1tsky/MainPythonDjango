a402360a0eabc0cb8b9ec0bacb701d03f1b73890
lev1tsky <levytskyi.denys@gmail.com>
2022-04-19 19:01:16 +0200

    update

---
+++ HW1.2_Calculator.py
@@ -0,0 +1,10 @@
+num1 = int(input("Напишите первое число: "))
+num2 = int(input("Напишите второе число: "))
+result1= num1+num2
+result2= num1-num2
+result3= num1*num2
+result4= num1/num2
+print("Результат сложения: ", result1)
+print("Результат вычитания: ", result2)
+print("Результат умножения: ", result3)
+print("Результат деления: ", result4)
